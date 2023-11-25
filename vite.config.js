911b39fd839e5523e46b47b90bf4023e447ba409
Jordan Golden <112906949+TheKarmicCoder@users.noreply.github.com>
2023-11-25 18:57:01 -0500

    build issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import styledComponents from 'babel-plugin-styled-components';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      include: /.(jsx|tsx)$/,
+      babel: {
+        plugins: [styledComponents],
+        babelrc: false,
+        configFile: false,
+      },
+    }),
+  ],
+});
